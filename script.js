7f3ee9c3b719baebd97179490378e6d18ceb8ffb
MartijnW29 <92314935+MartijnW29@users.noreply.github.com>
2021-11-03 11:37:45 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ function addToScore(amount) {
   document.getElementById("score").innerHTML = score;
 }
 
+function resetgame() {
+  if (comfirm("are you sure you want to restart")) {
+    var gamesave = {};
+    localstorage.setitem("gamesave",JSON.stringify(gamesave));
+    location.reload();
+  }
+}
+
   function buyAmber() {
     if (score >= Ambercost) {
       score = score - Ambercost;
@@ -49,10 +57,51 @@ function addToScore(amount) {
   var Kaeyacost = 1000;
   var Kaeyas = 0;
 
-  function updatescorepersecond(Cursors, Lisas, Kaeyas) {
+function loadgame() {
+  var savegame = JSON.parse(localstorage.getitem("gamesave"));
+if (typeof savedgame.score !== "undefined") score = savedgame.score;
+if (typeof savedgame.clickingPower !== "undefined") clickingPower = savedgame.clickingpower;
+if (typeof savedgame.Ambercost !== "undefined") clickingPower = savedgame.Ambercost;
+if (typeof savedgame.Ambers !== "undefined") clickingPower = savedgame.Ambers;
+if (typeof savedgame.Lisacost !== "undefined") clickingPower = savedgame.Lisacost;
+if (typeof savedgame.Lisas !== "undefined") clickingPower = savedgame.Lisas;
+if (typeof savedgame.Kaeyacost !== "undefined") clickingPower = savedgame.Kaeyacost;
+if (typeof savedgame.Kaeyas !== "undefined") clickingPower = savedgame.Kaeyas;
+
+}
+
+  function savegame() {
+    var gamesave = {
+      score: score,
+      clickingPower: clickingPower,
+      Ambercost: Ambercost,
+      Ambers: Ambers,
+      Lisacost: Lisacost,
+      Lisas: Lisas,
+      Kaeyacost: Kaeyacost,
+      Kaeyas: Kaeyas
+      //voeg achter elke een komma, toe maar niet de laatste
+    };
+    localstorage.setitem("gamesave", JSON.stringify(gamesave));
+  }
+
+  function updatescorepersecond(Ambers, Lisas, Kaeyas) {
     scorepersecond = Ambers + (Lisas * 5) + (Kaeyas * 70);
     document.getElementById("scorepersecond").innerHTML = scorepersecond;
   }
+
+window.onload = function () {
+  loadgame();
+  updatescorepersecond();
+  document.getElementById("score").innerHTML = score;
+  document.getElementById("Ambercost").innerHTML = Ambercost;
+  document.getElementById("Ambers").innerHTML = Ambers;
+  document.getElementById("Lisacost").innerHTML = Lisacost;
+  document.getElementById("Lisas").innerHTML = Lisas;
+  document.getElementById("Kaeyacost").innerHTML = Kaeyacost;
+  document.getElementById("Kaeyas").innerHTML = Kaeyas;
+
+};
 
   setInterval (function() {
     score = score + Ambers;
@@ -62,3 +111,14 @@ function addToScore(amount) {
 
     document.title = score + "-primogems - Genshin clicker"
   }, 1000) ; //1000ms = 1 second
+
+  setInterval(function() {
+    savegame ();
+  }, 30000);//30000ms = 30 secondes
+
+  document.addeventlistener("keydows", function(event) {
+    if (event.ctrlKey && event.which == 83) {//ctrl + s
+    event.preventDefault();
+    savegame();
+  }
+  }, flase);
